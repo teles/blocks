4a3e352a4895d49b3746214525913b2194ff6fd6
Teles <jose.maciel@vivadecora.com.br>
2022-07-29 22:52:00 -0300

    Adiciona componente basico

---
+++ components/BasicCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h2>{{ configs.title }}</h2>
+    <h3>{{ configs.subtitle }} </h3>
+    <div>
+      <p v-for="(block, index) in blocks" :key="index">
+        {{ block.text }}
+      </p>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    blocks: {
+      type: Array,
+      required: true
+    },
+    configs: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {}
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>oi</div>
+  <div>
+    <BasicCard
+      :configs="configs"
+      :blocks="blocks"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      configs: {
+        title: 'Exemplo de título',
+        subtitle: 'Exemplo de subtítulo'
+      },
+      blocks: [{
+        text: 'Primeiro parágrafo'
+      }, {
+        text: 'Segundo parágrafo'
+      }]
+    }
+  }
 })
 </script>
